f812c129849bd3854d06ec1418ef4fd7100d7187
Sh09507 <71281402+Sh09507@users.noreply.github.com>
2021-04-29 02:18:38 -0400

    Add files via upload

--- auto.py
+++ auto.py
@@ -20,8 +20,8 @@ if response.getcode() >= 200 and response.getcode() < 300:
 		print("Redirected to " + response.geturl())
 	html_doc = response.read()
 	soup = BeautifulSoup(html_doc, 'html.parser')
-	for link in soup.find_all('a'): # It helps to find all anchor tag's
-		print(link.get('href'))
+	for link in soup.find_all('form'): # It helps to find all anchor tag's
+		print(link.get('method'))
 # Note: urllib.urlopen automatically follows redirects (codes 300-399)
 elif response.getcode() >= 400 and response.getcode() < 500:
 	print(str(response.getcode()) + " - Client error")
