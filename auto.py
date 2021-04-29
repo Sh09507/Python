9f74c4e13b057f684cfe54d2b152301b6d7855a4
Sh09507 <71281402+Sh09507@users.noreply.github.com>
2021-04-29 03:15:21 -0400

    Add files via upload

--- auto.py
+++ auto.py
@@ -6,7 +6,123 @@ import time
 from bs4 import BeautifulSoup
 
 # Open the URL and get the response
-url = "https://sabrinahill.xyz/websecurity"
+url = "https://sabrinahill.xyz/websecurity/index.php"
+print("Opening " + url)
+start = time.clock()
+response = urllib.urlopen(url)
+elapsed = time.clock() - start
+print("Response code: " + str(response.getcode()))
+print("Response time: " + str(elapsed))
+
+# Check the status code and print results
+if response.getcode() >= 200 and response.getcode() < 300:
+	if response.geturl() != url:
+		print("Redirected to " + response.geturl())
+	html_doc = response.read()
+	soup = BeautifulSoup(html_doc, 'html.parser')
+	for link in soup.find_all('form'): # It helps to find all anchor tag's
+		print(link.get('method'))
+# Note: urllib.urlopen automatically follows redirects (codes 300-399)
+elif response.getcode() >= 400 and response.getcode() < 500:
+	print(str(response.getcode()) + " - Client error")
+	html_doc = response.read()
+	print(html_doc)
+elif response.getcode() >= 500 and response.getcode() < 600:
+	print(str(response.getcode()) + " - Server error")
+	html_doc = response.read()
+	print(html_doc)
+else:
+	print(str(response.getcode()))
+
+
+url = "https://sabrinahill.xyz/websecurity/login.php"
+print("Opening " + url)
+start = time.clock()
+response = urllib.urlopen(url)
+elapsed = time.clock() - start
+print("Response code: " + str(response.getcode()))
+print("Response time: " + str(elapsed))
+
+# Check the status code and print results
+if response.getcode() >= 200 and response.getcode() < 300:
+	if response.geturl() != url:
+		print("Redirected to " + response.geturl())
+	html_doc = response.read()
+	soup = BeautifulSoup(html_doc, 'html.parser')
+	for link in soup.find_all('form'): # It helps to find all anchor tag's
+		print(link.get('method'))
+# Note: urllib.urlopen automatically follows redirects (codes 300-399)
+elif response.getcode() >= 400 and response.getcode() < 500:
+	print(str(response.getcode()) + " - Client error")
+	html_doc = response.read()
+	print(html_doc)
+elif response.getcode() >= 500 and response.getcode() < 600:
+	print(str(response.getcode()) + " - Server error")
+	html_doc = response.read()
+	print(html_doc)
+else:
+	print(str(response.getcode()))
+
+
+url = "https://sabrinahill.xyz/websecurity/forgotpass.php"
+print("Opening " + url)
+start = time.clock()
+response = urllib.urlopen(url)
+elapsed = time.clock() - start
+print("Response code: " + str(response.getcode()))
+print("Response time: " + str(elapsed))
+
+# Check the status code and print results
+if response.getcode() >= 200 and response.getcode() < 300:
+	if response.geturl() != url:
+		print("Redirected to " + response.geturl())
+	html_doc = response.read()
+	soup = BeautifulSoup(html_doc, 'html.parser')
+	for link in soup.find_all('form'): # It helps to find all anchor tag's
+		print(link.get('method'))
+# Note: urllib.urlopen automatically follows redirects (codes 300-399)
+elif response.getcode() >= 400 and response.getcode() < 500:
+	print(str(response.getcode()) + " - Client error")
+	html_doc = response.read()
+	print(html_doc)
+elif response.getcode() >= 500 and response.getcode() < 600:
+	print(str(response.getcode()) + " - Server error")
+	html_doc = response.read()
+	print(html_doc)
+else:
+	print(str(response.getcode()))
+
+
+url = "https://sabrinahill.xyz/websecurity/registrarion.php"
+print("Opening " + url)
+start = time.clock()
+response = urllib.urlopen(url)
+elapsed = time.clock() - start
+print("Response code: " + str(response.getcode()))
+print("Response time: " + str(elapsed))
+
+# Check the status code and print results
+if response.getcode() >= 200 and response.getcode() < 300:
+	if response.geturl() != url:
+		print("Redirected to " + response.geturl())
+	html_doc = response.read()
+	soup = BeautifulSoup(html_doc, 'html.parser')
+	for link in soup.find_all('form'): # It helps to find all anchor tag's
+		print(link.get('method'))
+# Note: urllib.urlopen automatically follows redirects (codes 300-399)
+elif response.getcode() >= 400 and response.getcode() < 500:
+	print(str(response.getcode()) + " - Client error")
+	html_doc = response.read()
+	print(html_doc)
+elif response.getcode() >= 500 and response.getcode() < 600:
+	print(str(response.getcode()) + " - Server error")
+	html_doc = response.read()
+	print(html_doc)
+else:
+	print(str(response.getcode()))
+
+
+url = "https://sabrinahill.xyz/websecurity/topic.php"
 print("Opening " + url)
 start = time.clock()
 response = urllib.urlopen(url)
